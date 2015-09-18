de4d5ca7889376933becc5bf908c06a3c7ff5ada
Vasa <vasanth.krishnamoorthy@citrix.com>
2015-09-18 07:00:06 -0700

    Hydrating client with state from the server

--- client/index.jsx
+++ client/index.jsx
@@ -2,10 +2,28 @@ import React from 'react';
 import { Router } from 'react-router';
 import createBrowserHistory from 'history/lib/createBrowserHistory';
 import routes from 'routes';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import * as reducers from 'reducers';
+import { fromJS } from 'immutable';
 
 const history = createBrowserHistory();
 
+let initialState = window.__INITIAL_STATE__;
+
+// Transform into Immutable.js collections, but leave top level keys untouched in Redux.
+Object.keys(initialState).forEach(key => {
+  initialState[key] = fromJS(initialState[key]);
+});
+
+const reducer = combineReducers(reducers);
+const store = createStore(reducer, initialState);
+
 React.render(
-  <Router children={routes} history={history}/>,
+  <Provider store={store}>
+    {() =>
+      <Router children={routes} history={history}/>
+    }
+  </Provider>,
   document.getElementById('react-view')
 );
