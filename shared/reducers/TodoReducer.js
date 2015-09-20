3d04aabb3ff56051c970f1c5bbf6fad4659a8770
Vasa <vasanth.krishnamoorthy@citrix.com>
2015-09-19 20:43:52 -0700

    Added GET_TODOS to reducer

--- shared/reducers/TodoReducer.js
+++ shared/reducers/TodoReducer.js
@@ -1,9 +1,13 @@
 import Immutable from 'immutable';
+
 const defaultState = new Immutable.List();
+
 export default function todoReducer(state = defaultState, action = '') {
-  switch (action.type) {
+  switch(action.type) {
+    case 'GET_TODOS':
+      return state.concat(action.res.data);
     case 'CREATE_TODO':
-      return state.concat(action.text);
+      return state.concat(action.res.data.text);
     case 'EDIT_TODO':
       return state.set(action.id, action.text);
     case 'DELETE_TODO':
