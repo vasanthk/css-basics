74d543bb06b07d7ce8e46565aea5abf5fafad5b8
Vasa <vasanth.krishnamoorthy@citrix.com>
2015-09-19 20:34:30 -0700

    Updated Hom component

--- shared/components/Home.jsx
+++ shared/components/Home.jsx
@@ -1,16 +1,25 @@
-import React from 'react';
-import TodosView from 'components/TodosView';
-import TodosForm from 'components/TodosForm';
+import React, { PropTypes }   from 'react';
+import TodosView              from './TodosView';
+import TodosForm              from './TodosForm';
 import { bindActionCreators } from 'redux';
-import * as TodoActions from 'actions/TodoActions';
-import { connect } from 'react-redux';
+import * as TodoActions       from 'actions/TodoActions';
+import { connect }            from 'react-redux';
 
 // Decorator:
 // Redux’s @connect decorator wraps our class in another component ( <Connector>),
 // giving it access to the requested parts of state as props, hence why we can use todos as we do.
-@connect(state => ({todos: state.todos}))
-export default
-class Home extends React.Component {
+@connect(state => ({ todos: state.todos }))
+
+export default class Home extends React.Component {
+  static propTypes = {
+    todos:    PropTypes.any.isRequired,
+    dispatch: PropTypes.func.isRequired
+  };
+
+  static needs = [
+    TodoActions.getTodos
+  ];
+
   render() {
     const { todos, dispatch } = this.props;
 
@@ -18,8 +27,9 @@ class Home extends React.Component {
       <div id="todo-list">
         <TodosView todos={todos}
           {...bindActionCreators(TodoActions, dispatch)} />
+
         <TodosForm
-          {...bindActionCreators(TodoActions, dispatch)} />
+          {...bindActionCreators(TodoActions, dispatch)}/>
       </div>
     );
   }
