435f44f3b82c0f5b494fb5a564698114d59033b9
Vasa <vasanth.krishnamoorthy@citrix.com>
2015-09-19 20:35:55 -0700

    Updated actions with promises

--- shared/actions/TodoActions.js
+++ shared/actions/TodoActions.js
@@ -1,10 +1,21 @@
+import request from 'axios';
+
+const API_URL = 'https://webtask.it.auth0.com/api/run/wt-milomord-gmail_com-0/redux-tutorial-backend?webtask_no_cache=1';
+
+export function getTodos() {
+  return {
+    type: 'GET_TODOS',
+    promise: request.get(API_URL)
+  }
+}
+
 export function createTodo(text) {
   return {
     type: 'CREATE_TODO',
-    text,
-    date: Date.now()
-  }
+    promise: request.post(API_URL, { time: Date.now(), text })
+  };
 }
+
 export function editTodo(id, text) {
   return {
     type: 'EDIT_TODO',
@@ -13,6 +24,7 @@ export function editTodo(id, text) {
     date: Date.now()
   };
 }
+
 export function deleteTodo(id) {
   return {
     type: 'DELETE_TODO',
