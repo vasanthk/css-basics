06e5bce8f17ce119a6879490de00cc924c9e7bae
Vasa <vasanth.krishnamoorthy@citrix.com>
2015-09-19 23:15:12 -0700

    Note on Decorators

--- shared/components/Home.jsx
+++ shared/components/Home.jsx
@@ -8,6 +8,15 @@ import { connect }            from 'react-redux';
 // Decorator:
 // Redux’s @connect decorator wraps our class in another component ( <Connector>),
 // giving it access to the requested parts of state as props, hence why we can use todos as we do.
+
+// Details:
+// The @connect decorator is a factory function that takes a function as an argument and returns an actual decorator function.
+// The returned function then converts a state from Redux to props of the Component - every time state changes.
+// This is amazing because our component does not need to handle any Redux state change. All it needs to do is, being fed with new props.
+
+// The connect decorator hides all the ugly and unmaintainable state transition codes as its implementation details.
+// It exposes a simple, readable and easy to understand transform function.
+// Reference link: https://medium.com/@tomchentw/redux-universal-576fb9475b5b
 @connect(state => ({ todos: state.todos }))
 
 export default class Home extends React.Component {
