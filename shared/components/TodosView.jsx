22ef6169eeb79a6122e559d1f878bac5ea66e42d
Vasa <vasanth.krishnamoorthy@citrix.com>
2015-09-19 20:40:56 -0700

    Styles + proptype validations

--- shared/components/TodosView.jsx
+++ shared/components/TodosView.jsx
@@ -1,6 +1,14 @@
-import React from 'react';
+import React         from 'react';
+import { PropTypes } from 'react';
+import Immutable     from 'immutable';
 
 export default class TodosView extends React.Component {
+  static propTypes = {
+    todos: PropTypes.instanceOf(Immutable.List).isRequired,
+    editTodo: PropTypes.func.isRequired,
+    deleteTodo: PropTypes.func.isRequired
+  };
+
   // note that we use arrow functions in the class definition,
   // so that this will be bound to the class’ constructor (since arrow functions inherit the scope’s this)
   handleDelete = (e) => {
@@ -12,33 +20,33 @@ export default class TodosView extends React.Component {
 
   handleEdit = (e) => {
     const id = Number(e.target.dataset.id);
-    const val = this.props.todos.get(id).text;
+    const currentVal = this.props.todos.get(id).text;
 
     // For cutting-edge UX
-    let newVal = window.prompt(', val');
-    this.props.editTodo(id, newVal);
+    let text = window.prompt('', currentVal);
+
+    this.props.editTodo(id, text);
   };
 
   render() {
+    const btnStyle = {
+      'margin': '1em 0 1em 1em'
+    };
+
     return (
-      <div id="todo-list">
+      <div id="todos-list">
         {
           this.props.todos.map((todo, index) => {
             return (
-              <div key={index}>
+              <div style={btnStyle} key={index}>
                 <span>{todo}</span>
-
-                <button data-id={index} onClick={this.handleDelete}>
-                  X
-                </button>
-                <button data-id={index} onClick={this.handleEdit}>
-                  Edit
-                </button>
+                <button style={btnStyle} data-id={index} onClick={this.handleDelete}>X</button>
+                <button style={btnStyle} data-id={index} onClick={this.handleEdit}>Edit</button>
               </div>
-            )
+            );
           })
         }
       </div>
-    )
+    );
   }
 }
